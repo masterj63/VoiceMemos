be97f1868a8d70603c4a0eaa966b0d6c281a073e
Noname Sorry <noemail@sor.ry>
2014-05-07 15:24:28 +0400

    +basic menu

--- VoiceMemos/src/mdev/master_j/voicememos/MainActivity.java
+++ VoiceMemos/src/mdev/master_j/voicememos/MainActivity.java
@@ -1,14 +1,31 @@
 package mdev.master_j.voicememos;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.item_add_memo)
+			;
+		return super.onOptionsItemSelected(item);
 	}
 
 }
