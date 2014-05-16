525b3933c61a813c66e383e8bef3e9f897b88be7
Noname Sorry <noemail@sor.ry>
2014-05-16 18:25:52 +0400

    +recordProgressBar updating

--- VoiceMemos/src/mdev/master_j/voicememos/MemoCreaterDialog.java
+++ VoiceMemos/src/mdev/master_j/voicememos/MemoCreaterDialog.java
@@ -1,13 +1,18 @@
 package mdev.master_j.voicememos;
 
+import java.io.File;
+import java.io.IOException;
+
 import mdev.master_j.voicememos.MainActivity.Memo;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,9 +21,10 @@ import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MemoCreaterDialog extends DialogFragment {
-	private static final int DURATION_ERROR_MS = 1000;
+	private static final int PERIOD_RECORD_PROGRESS_UPDATE_MS = 200;
 	private static final int DURATION_RECORD_MS = 10 * 1000;
 	private static final int DURATION_PROGRESSBAR_FADE_IN_MS = 1000;
 
@@ -31,15 +37,17 @@ public class MemoCreaterDialog extends DialogFragment {
 	private Button positiveButton;
 	private Button neutralButton;
 
+	private RecordState recordState = RecordState.IDLE;
+
 	ProgressBar recordProgressBar;
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		mainActivity = (MainActivity) getActivity();
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
 
-		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LayoutInflater inflater = (LayoutInflater) mainActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		dialogView = inflater.inflate(R.layout.memo_creator, null, false);
 
 		builder.setView(dialogView);
@@ -57,6 +65,8 @@ public class MemoCreaterDialog extends DialogFragment {
 		super.onStart();
 
 		recordProgressBar = (ProgressBar) dialogView.findViewById(R.id.progress_bar_record);
+		recordProgressBar.setMax(DURATION_RECORD_MS);
+		recordProgressBar.setProgress(0);
 
 		memoNameEditText = (EditText) dialogView.findViewById(R.id.name_memo_dialog);
 		memoNameEditText.addTextChangedListener(textWatcher);
@@ -73,15 +83,63 @@ public class MemoCreaterDialog extends DialogFragment {
 	private OnClickListener onNeutralButtonClickListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			neutralButton.setText(R.string.button_record_stop);
-
-			Animation fadeInAnimation = new AlphaAnimation(0.0f, 1.0f);
-			fadeInAnimation.setDuration(DURATION_PROGRESSBAR_FADE_IN_MS);
-			fadeInAnimation.setStartOffset(0);
-			recordProgressBar.startAnimation(fadeInAnimation);
-			recordProgressBar.findViewById(R.id.progress_bar_record).setVisibility(View.VISIBLE);
+			switch (recordState) {
+			case IDLE:
+				onStartRecord();
+				break;
+			case IN_PROGRESS:
+				break;
+			case RECORDED:
+				break;
+			default:
+				Log.d("mj", "recordState==null?");
+			}
 		}
 	};
+
+	private void onStartRecord() {
+		recordState = RecordState.IN_PROGRESS;
+
+		neutralButton.setText(R.string.button_record_stop);
+
+		Animation fadeInAnimation = new AlphaAnimation(0.0f, 1.0f);
+		fadeInAnimation.setDuration(DURATION_PROGRESSBAR_FADE_IN_MS);
+		fadeInAnimation.setStartOffset(0);
+		recordProgressBar.startAnimation(fadeInAnimation);
+		recordProgressBar.findViewById(R.id.progress_bar_record).setVisibility(View.VISIBLE);
+
+		MediaRecorder recorder = new MediaRecorder();
+		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+		String path = mainActivity.getFilesDir() + "/" + memoNameEditText.getText().toString();
+		File outputFile = new File(path);
+		if (outputFile.exists())
+			outputFile.delete();
+		try {
+			outputFile.createNewFile();
+		} catch (IOException e) {
+			Toast.makeText(mainActivity, e.toString(), Toast.LENGTH_LONG).show();
+			Log.e("mj", e.toString());
+			e.printStackTrace();
+			return;
+		}
+		recorder.setOutputFile(path);
+
+		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+		try {
+			recorder.prepare();
+		} catch (Throwable e) {
+			Toast.makeText(mainActivity, e.toString(), Toast.LENGTH_LONG).show();
+			Log.e("mj", e.toString());
+			e.printStackTrace();
+			return;
+		}
+
+		new Thread(recordProgressUpdaterRunnable).start();
+		recorder.start();
+	}
 
 	private OnClickListener onPositiveButtonClickListener = new OnClickListener() {
 		@Override
@@ -148,4 +206,26 @@ public class MemoCreaterDialog extends DialogFragment {
 			memoNameEditText.setError(getString(R.string.error_too_many_similar_memos));
 		}
 	};
+
+	private Runnable recordProgressUpdaterRunnable = new Runnable() {
+		@Override
+		public void run() {
+			try {
+				int progress = 0;
+				while (progress <= DURATION_RECORD_MS) {
+					progress += PERIOD_RECORD_PROGRESS_UPDATE_MS;
+					recordProgressBar.setProgress(progress);
+					Thread.sleep(PERIOD_RECORD_PROGRESS_UPDATE_MS);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				Log.e("mj", e.toString());
+			}
+			// TODO on finish
+		}
+	};
+
+	private enum RecordState {
+		IDLE, IN_PROGRESS, RECORDED
+	}
 }
